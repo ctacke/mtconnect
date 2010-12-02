c152314ea605f2bfa7dc566e5a26a04c01f2e749
ctacke_cp <SND\ctacke_cp>
2010-12-02 15:22:40 +0000

    initial unit test project check-in

---
+++ OpenNETCF.Net.MTConnect.Test/AdapterCollectionTest.cs
@@ -0,0 +1,27 @@
+﻿using OpenNETCF.Net.MTConnect;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OpenNETCF.Net.MTConnect.Test
+{
+    [TestClass()]
+    public class AdapterCollectionTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod()]
+        public void AddOrgAdapterTest()
+        {
+            Agent agent = new Agent();
+            Adapter a = new OrgAdapter();
+
+            var start = agent.Adapters.Count;
+
+            agent.Adapters.Add(a);
+
+            Assert.AreEqual(start + 1, agent.Adapters.Count);
+
+        }
+
+    }
+}
