b7e91bfde9c16b487f41d5bceda1cc321e3e094f
ctacke_cp <SND\ctacke_cp>
2011-04-28 18:35:49 +0000

    Updates for WinPhone

---
+++ OpenNETCF.MTConnect.Common/Extensions/ComponentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace OpenNETCF.MTConnect
+{
+    public static class ComponentExtensions
+    {
+        public static Component FirstRecursive(this IEnumerable<Component> list, Func<Component, bool> func)
+        {
+            foreach (var component in list)
+            {
+                if (func(component)) return component;
+            }
+
+            foreach (var component in list)
+            {
+                var comp = component.Components.FirstRecursive(func);
+                if (comp != null) return comp;
+            }
+
+            return null;
+        }
+    }
+}
